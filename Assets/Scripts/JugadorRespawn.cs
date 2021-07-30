bafed3ed5f3649841c3282c2938962fd7c4487b7
anthony flor velasquez <antony9015@gmail.com>
2021-07-30 14:16:51 -0500

    control de vidas de juugador

--- Assets/Scripts/JugadorRespawn.cs
+++ Assets/Scripts/JugadorRespawn.cs
@@ -6,9 +6,40 @@ public class JugadorRespawn : MonoBehaviour
 {
     public Animator animator; 
     public GameObject Panel;
+
+
+    public GameObject[] hearts;
+    private int life;
+    public AudioSource AudioHit;
+    void Start(){
+        life = hearts.Length;
+    }
+    public void CheckLife(){
+        if(life < 1){
+            Destroy(hearts[0].gameObject);
+            animator.Play("death");
+            Panel.SetActive(true);
+            DisabledJugador();
+            //GetComponent<Jugador>().enabled=false;
+        }else if(life < 2){
+            Destroy(hearts[1].gameObject);
+            animator.Play("hit");
+            AudioHit.Play();
+        }else if(life < 3){
+            Destroy(hearts[2].gameObject);
+            animator.Play("hit");
+            AudioHit.Play();
+        }
+    }
+
     public void PlayerDamage(){
-        animator.Play("death");
-        Panel.SetActive(true);
+        life--;
+        CheckLife();
+        //animator.Play("death");
+        //Panel.SetActive(true);
+        //GetComponent<Jugador>().enabled=false;
+    }
+    public void DisabledJugador(){
         GetComponent<Jugador>().enabled=false;
     }
 }
